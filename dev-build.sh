e5a59d1249f853568dac45039d8142f23c9dc55c
Paul MARS <paul.mars@intrinsec.com>
2017-08-10 14:06:17 +0200

    Adapt docker args to save the report.

--- dev-build.sh
+++ dev-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t intrinsec/comission .
-docker run -it --rm -v /root/wpchecker-test-data/:/cms_path intrinsec/comission -d /cms_path
+docker build -t isec/comission .
+docker run -it --rm -v /root/wpchecker-test-data/:/cms_path/ -v /root/:/output/ isec/comission -d /cms_path/ -c wordpress -o /output/test.xlsx
