be8828d0543d3447274ffedeacd23766b0828324
pms <paul.mars@intrinsec.com>
2019-06-10 15:28:38 +0200

    Corrections on args description

--- comission/utilsCMS.py
+++ comission/utilsCMS.py
@@ -60,7 +60,7 @@ def parse_args() -> Dict:
     parser.add_argument(
         "--wp-content",
         dest="wp_content",
-        help="Set this to force the wp-content directory location.",
+        help="Set this to force the wp-content directory location. (WordPress only)",
     )
     parser.add_argument(
         "--plugins-dir",
@@ -75,7 +75,7 @@ def parse_args() -> Dict:
     parser.add_argument(
         "--major",
         dest="version_major",
-        help="Specify the core major version (eg. 7, 8) when using --skip-core arg. Works only for Drupal.",
+        help="Specify the core major version (eg. 7, 8) when using --skip-core arg. (Drupal only)",
     )
     parser.add_argument(
         "-v",
@@ -84,7 +84,7 @@ def parse_args() -> Dict:
         help="Specify the core full version (eg. 5.5).",
     )
     parser.add_argument(
-        "--wpvulndb-token", dest="wpvulndb_token", help="Set a token to request wpvulndb API."
+        "--wpvulndb-token", dest="wpvulndb_token", help="Set a token to request wpvulndb API. (WordPress only)"
     )
     parser.add_argument(
         "--debug",
