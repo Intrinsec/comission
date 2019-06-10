97d2e8eceee7f7b62417de2b67dcf4d8f3c017d3
pms <paul.mars@intrinsec.com>
2019-06-10 17:27:20 +0200

    Extract dataset from tests

---
+++ tests/dataset.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+
+from comission.CMS.models.Vulnerability import Vulnerability
+from comission.CMS.models.Core import Core
+from comission.CMS.models.Alteration import Alteration
+from comission.CMS.models.Addon import Addon
+
+"""
+A dataset to test various stage of a CMS analysis
+"""
+
+class DataSet:
+    def __init__(self):
+
+        # Dataset specific to WordPress
+        self.addon_wp_stage0 = Addon()
+        self.addon_wp_stage0.type = "plugins"
+        self.addon_wp_stage0.name = "w3-total-cache"
+        self.addon_wp_stage0.filename = ""
+        self.addon_wp_stage0.version = ""
+        self.addon_wp_stage0.notes = ""
+        self.addon_wp_stage0.subtype = "mu"
+
+        self.addon_wp_stage1 = Addon()
+        self.addon_wp_stage1.type = "plugins"
+        self.addon_wp_stage1.name = "w3-total-cache"
+        self.addon_wp_stage1.filename = "w3-total-cache.php"
+        self.addon_wp_stage1.version = ""
+        self.addon_wp_stage1.notes = ""
+        self.addon_wp_stage1.subtype = ""
+
+        self.addon_wp_stage2 = Addon()
+        self.addon_wp_stage2.type = "plugins"
+        self.addon_wp_stage2.name = "w3-total-cache"
+        self.addon_wp_stage2.last_version = ""
+        self.addon_wp_stage2.last_release_date = ""
+        self.addon_wp_stage2.link = ""
+        self.addon_wp_stage2.version = "0.9.4.1"
+        self.addon_wp_stage2.subtype = ""
+        self.addon_wp_stage2.vulns = []
+        self.addon_wp_stage2.alterations = []
+
+        # Dataset specific to Drupal analysis
+
+        self.addon_dpl_stage1 = Addon()
+        self.addon_dpl_stage1.type = "plugins"
+        self.addon_dpl_stage1.name = "xmlsitemap"
+        self.addon_dpl_stage1.filename = "xmlsitemap.info"
+        self.addon_dpl_stage1.version = ""
+        self.addon_dpl_stage1.notes = ""
+
+        self.addon_dpl_stage2 = Addon()
+        self.addon_dpl_stage2.type = "plugins"
+        self.addon_dpl_stage2.name = "media_youtube"
+        self.addon_dpl_stage2.last_version = ""
+        self.addon_dpl_stage2.last_release_date = ""
+        self.addon_dpl_stage2.link = ""
+        self.addon_dpl_stage2.version = "7.x-3.4"
+        self.addon_dpl_stage2.notes = ""
+        self.addon_dpl_stage2.alterations = []
+
+        self.alteration = Alteration()
+        self.alteration.type = ""
+        self.alteration.target = ""
+        self.alteration.file = ""
+        self.alteration.type = ""
+
+        self.vuln = Vulnerability()
+        self.vuln.name = "Vuln name"
+        self.vuln.link = ""
+        self.vuln.type = ""
+        self.vuln.poc = ""
+        self.vuln.fixed_in = ""
+
+        self.core = Core()
+        self.core.version = "4.5.1"
+        self.core.last_version = "4.8"
+        self.core.version_major = "4"
+        self.core.alterations = [self.alteration, self.alteration, self.alteration]
+        self.core.vulns = [self.vuln, self.vuln, self.vuln]
+
+        self.plugin = Addon()
+        self.plugin.type = "plugins"
+        self.plugin.name = "Name plugin"
+        self.plugin.version = "1.0"
+        self.plugin.last_version = "2.0"
+        self.plugin.last_release_date = "2017-08-25"
+        self.plugin.link = "https://test.link.addon"
+        self.plugin.edited = "YES"
+        self.plugin.subtype = ""
+        self.plugin.cve = "YES"
+        self.plugin.vulns = [self.vuln, self.vuln, self.vuln]
+        self.plugin.notes = ""
+        self.plugin.alterations = [self.alteration, self.alteration, self.alteration]
+        self.plugin.filename = ""
+
+        self.muplugin = Addon()
+        self.muplugin.type = "plugins"
+        self.muplugin.name = "Name plugin"
+        self.muplugin.version = "1.0"
+        self.muplugin.last_version = "2.0"
+        self.muplugin.last_release_date = "2017-08-25"
+        self.muplugin.link = "https://test.link.addon"
+        self.muplugin.edited = "YES"
+        self.muplugin.subtype = "mu"
+        self.muplugin.cve = "YES"
+        self.muplugin.vulns = [self.vuln, self.vuln, self.vuln]
+        self.muplugin.notes = ""
+        self.muplugin.alterations = [self.alteration, self.alteration, self.alteration]
+        self.muplugin.filename = ""
+
+        self.theme = Addon()
+        self.theme.type = "themes"
+        self.theme.name = "Name theme"
+        self.theme.version = "1.0"
+        self.theme.last_version = "2.0"
+        self.theme.last_release_date = "2017-08-25"
+        self.theme.link = "https://test.link.addon"
+        self.theme.edited = "YES"
+        self.theme.subtype = ""
+        self.theme.cve = "YES"
+        self.theme.vulns = [self.vuln, self.vuln, self.vuln]
+        self.theme.notes = ""
+        self.theme.alterations = [self.alteration, self.alteration, self.alteration]
+        self.theme.filename = ""
+
+        self.plugins = [self.plugin, self.plugin, self.muplugin]
+        self.themes = [self.theme for _ in range(3)]
+
--- tests/testCMS.py
+++ tests/testCMS.py
@@ -11,129 +11,7 @@ import comission.CMS.Drupal as Drupal
 import comission.CMS.WordPress as WordPress
 import comission.reportCMS as rCMS
 import comission.utilsCMS as uCMS
-from comission.CMS.models.Vulnerability import Vulnerability
-from comission.CMS.models.Core import Core
-from comission.CMS.models.Alteration import Alteration
-from comission.CMS.models.Addon import Addon
-
-
-class DataSet:
-    def __init__(self):
-
-        # Dataset specific to WordPress
-        self.addon_wp_stage0 = Addon()
-        self.addon_wp_stage0.type = "plugins"
-        self.addon_wp_stage0.name = "w3-total-cache"
-        self.addon_wp_stage0.filename = ""
-        self.addon_wp_stage0.version = ""
-        self.addon_wp_stage0.notes = ""
-        self.addon_wp_stage0.subtype = "mu"
-
-        self.addon_wp_stage1 = Addon()
-        self.addon_wp_stage1.type = "plugins"
-        self.addon_wp_stage1.name = "w3-total-cache"
-        self.addon_wp_stage1.filename = "w3-total-cache.php"
-        self.addon_wp_stage1.version = ""
-        self.addon_wp_stage1.notes = ""
-        self.addon_wp_stage1.subtype = ""
-
-        self.addon_wp_stage2 = Addon()
-        self.addon_wp_stage2.type = "plugins"
-        self.addon_wp_stage2.name = "w3-total-cache"
-        self.addon_wp_stage2.last_version = ""
-        self.addon_wp_stage2.last_release_date = ""
-        self.addon_wp_stage2.link = ""
-        self.addon_wp_stage2.version = "0.9.4.1"
-        self.addon_wp_stage2.subtype = ""
-        self.addon_wp_stage2.vulns = []
-        self.addon_wp_stage2.alterations = []
-
-        # Dataset specific to Drupal analysis
-
-        self.addon_dpl_stage1 = Addon()
-        self.addon_dpl_stage1.type = "plugins"
-        self.addon_dpl_stage1.name = "xmlsitemap"
-        self.addon_dpl_stage1.filename = "xmlsitemap.info"
-        self.addon_dpl_stage1.version = ""
-        self.addon_dpl_stage1.notes = ""
-
-        self.addon_dpl_stage2 = Addon()
-        self.addon_dpl_stage2.type = "plugins"
-        self.addon_dpl_stage2.name = "media_youtube"
-        self.addon_dpl_stage2.last_version = ""
-        self.addon_dpl_stage2.last_release_date = ""
-        self.addon_dpl_stage2.link = ""
-        self.addon_dpl_stage2.version = "7.x-3.4"
-        self.addon_dpl_stage2.notes = ""
-        self.addon_dpl_stage2.alterations = []
-
-        self.alteration = Alteration()
-        self.alteration.type = ""
-        self.alteration.target = ""
-        self.alteration.file = ""
-        self.alteration.type = ""
-
-        self.vuln = Vulnerability()
-        self.vuln.name = "Vuln name"
-        self.vuln.link = ""
-        self.vuln.type = ""
-        self.vuln.poc = ""
-        self.vuln.fixed_in = ""
-
-        self.core = Core()
-        self.core.version = "4.5.1"
-        self.core.last_version = "4.8"
-        self.core.version_major = "4"
-        self.core.alterations = [self.alteration, self.alteration, self.alteration]
-        self.core.vulns = [self.vuln, self.vuln, self.vuln]
-
-        self.plugin = Addon()
-        self.plugin.type = "plugins"
-        self.plugin.name = "Name plugin"
-        self.plugin.version = "1.0"
-        self.plugin.last_version = "2.0"
-        self.plugin.last_release_date = "2017-08-25"
-        self.plugin.link = "https://test.link.addon"
-        self.plugin.edited = "YES"
-        self.plugin.subtype = ""
-        self.plugin.cve = "YES"
-        self.plugin.vulns = [self.vuln, self.vuln, self.vuln]
-        self.plugin.notes = ""
-        self.plugin.alterations = [self.alteration, self.alteration, self.alteration]
-        self.plugin.filename = ""
-
-        self.muplugin = Addon()
-        self.muplugin.type = "plugins"
-        self.muplugin.name = "Name plugin"
-        self.muplugin.version = "1.0"
-        self.muplugin.last_version = "2.0"
-        self.muplugin.last_release_date = "2017-08-25"
-        self.muplugin.link = "https://test.link.addon"
-        self.muplugin.edited = "YES"
-        self.muplugin.subtype = "mu"
-        self.muplugin.cve = "YES"
-        self.muplugin.vulns = [self.vuln, self.vuln, self.vuln]
-        self.muplugin.notes = ""
-        self.muplugin.alterations = [self.alteration, self.alteration, self.alteration]
-        self.muplugin.filename = ""
-
-        self.theme = Addon()
-        self.theme.type = "themes"
-        self.theme.name = "Name theme"
-        self.theme.version = "1.0"
-        self.theme.last_version = "2.0"
-        self.theme.last_release_date = "2017-08-25"
-        self.theme.link = "https://test.link.addon"
-        self.theme.edited = "YES"
-        self.theme.subtype = ""
-        self.theme.cve = "YES"
-        self.theme.vulns = [self.vuln, self.vuln, self.vuln]
-        self.theme.notes = ""
-        self.theme.alterations = [self.alteration, self.alteration, self.alteration]
-        self.theme.filename = ""
-
-        self.plugins = [self.plugin, self.plugin, self.muplugin]
-        self.themes = [self.theme for _ in range(3)]
+from .dataset import DataSet
 
 
 class TestWordPressAnalysis(unittest.TestCase):
@@ -236,25 +114,17 @@ class TestWordPressAnalysis(unittest.TestCase):
         self.assertEqual(self.cms.plugins[0].version, "0.2")
 
 
-class TestDrupalAnalysis(unittest.TestCase):
+class TestDrupal7Analysis(unittest.TestCase):
     def setUp(self):
         self.dir_path = os.path.join(
             os.path.dirname(os.path.realpath(__file__)), "../test-data-set", "drupal", "drupal-7.X"
         )
-        self.cms = Drupal.DPL(self.dir_path, "", "")
+        self.cms = Drupal.Drupal7.DPL7(self.dir_path, "", "")
 
-    def test_get_core_version_DPL7(self):
+    def test_get_core_version(self):
         self.cms.get_core_version()
 
         self.assertEqual(self.cms.core.version, "7.56")
-
-    def test_get_core_versionDPL8(self):
-        self.cms.dir_path = os.path.join(
-            os.path.dirname(os.path.realpath(__file__)), "../test-data-set", "drupal", "drupal-8.X"
-        )
-        self.cms.get_core_version()
-
-        self.assertEqual(self.cms.core.version, "8.3.7")
 
     def test_get_addon_version(self):
         regex = re.compile("version = (.*)")
@@ -304,18 +174,38 @@ class TestDrupalAnalysis(unittest.TestCase):
 
         self.assertIn("media_youtube.test", altered_files)
 
-    def test_check_vulns_core(self):
+class TestDrupal8Analysis(unittest.TestCase):
+    def setUp(self):
+        self.dir_path = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), "../test-data-set", "drupal", "drupal-8.X"
+        )
+        self.cms = Drupal.Drupal8.DPL8(self.dir_path, "", "")
+
+    def test_get_core_version(self):
+        self.cms.get_core_version()
+
+        self.assertEqual(self.cms.core.version, "8.3.7")
+
+    def test_get_addon_version(self):
+        #TODO
         pass
 
-    def test_check_vulns_addon(self):
+    def test_get_core_last_version(self):
+        self.cms.core.version_major = "8"
+        self.cms.get_core_last_version()
+
+        self.assertEqual("8.7.3", self.cms.core.last_version)
+
+    def test_get_addon_last_version(self):
+        #TODO
         pass
 
-    def test_core_analysis(self):
-        pass
+    def test_check_core_alteration(self):
+        self.cms.core.version = "8.3.7"
+        download_core_url = "https://ftp.drupal.org/files/projects/drupal-8.3.7.zip"
+        alterations = self.cms.check_core_alteration(download_core_url)
 
-    def test_addon_analysis(self):
-        pass
-
+        self.assertEqual(alterations[0].file, "cron.php")
 
 class TestReportXLSX(unittest.TestCase):
     def setUp(self):
@@ -330,13 +220,16 @@ class TestReportXLSX(unittest.TestCase):
         self.workbook = load_workbook(report_name)
 
     def test_add_data(self):
+        #TODO
         pass
 
     def test_generate_heading(self):
+        #TODO
         self.workbook.get_sheet_names()
         pass
 
     def test_generate_formatting(self):
+        #TODO
         pass
 
 
